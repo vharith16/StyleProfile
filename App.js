9a6883c7eb96b4fb257c0de95345924ce5351682
vharith16 <97926605+vharith16@users.noreply.github.com>
2025-08-11 19:56:16 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Feed from './components/Feed';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Feed />
+    </div>
+  );
+}
+
+export default App;
